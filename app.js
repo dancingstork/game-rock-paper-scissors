ff05fefb01fd378021ef8cc549e0904640a93296
Klaudia <klaudiadevv@gmail.com>
2023-09-06 22:56:39 +0200

    added a game round player vs pc

--- app.js
+++ app.js
@@ -6,13 +6,37 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
 
     if (randomNumber === 0) {
-        return "computer pick: rock!"
+        return "rock"
     } else if (randomNumber === 1) {
-        return "computer pick: paper!"
+        return "paper"
     } else if (randomNumber === 2) {
-        return "computer pick: scissors!"
+        return "scissors"
     } else {}
-
 }
 
-console.log(getComputerChoice());
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("rock, paper or scissors?").toLowerCase();
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "it's a tie!"
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return `You lose! paper beats rock`
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return `you win! paper beats rock!`
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return `you win! paper beats rock!`
+
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return `you lose! scissors beat paper!`
+
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return `you lose! rock beats scissors!`
+
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "you win! scissors beat paper!"
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
